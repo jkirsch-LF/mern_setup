cc725ffc1d5d964f0fd1e16b03c78ca3b60821d2
James Kirsch <jameskirschaf@gmail.com>
2018-02-12 12:53:44 -0800

    Added pages

---
+++ client/src/components/api_test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ApiTest extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>API THUNDER DOME</h1>
+            </div>
+        )
+    }
+}
+
+export default ApiTest;
---
+++ client/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>This is Home</h1>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio dolorum repudiandae facere itaque temporibus provident laboriosam aspernatur ad necessitatibus sit nam quisquam similique commodi alias cupiditate suscipit officiis quae, quia nisi, architecto reprehenderit mollitia. Dolor rem similique beatae error autem. Magnam sapiente reprehenderit placeat consequuntur fugiat at expedita, totam illum laboriosam. Earum non officia commodi voluptate alias praesentium amet voluptates cum! Adipisci mollitia similique sed itaque consequuntur distinctio, natus nisi hic! Similique quam quidem animi delectus veniam molestiae, soluta earum laborum accusamus fuga placeat iusto aspernatur unde molestias provident eligendi dolores, maiores vero. Temporibus qui debitis, itaque exercitationem illum quis.</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
