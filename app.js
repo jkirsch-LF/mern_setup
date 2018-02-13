d4b6e2f01e7b4bb80937afe5dbe9d637147f89fa
James Kirsch <jameskirschaf@gmail.com>
2018-02-12 16:19:20 -0800

    Setup server for react deploy

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('<h1>boop</h1>');
-});
+app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>boop</h1>');
+// });
 
 app.get('/test-route', (req,res)=>{
     res.send("<h1>That's one-a bigga boopipty boop</h1>")
@@ -17,7 +21,11 @@ app.get('/api/get-user', cors(), (req, res)=>{
 
 app.get('/api/get-article', cors(), (req, res)=>{
     res.send({title: "Biggest Meatballs", content:"Like, the most meat-a-balls you've ever seen", meatballs:"Biggest"})
-})
+});
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+});
 
 // LISTEN LINDA
 const PORT = process.env.PORT || 9000;
