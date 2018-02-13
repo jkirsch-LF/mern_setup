5ba3514af95fab964f8d6356e06be13c6c664c5c
James Kirsch <jameskirschaf@gmail.com>
2018-02-12 16:05:32 -0800

    Added axios calls and routing to handle calls

--- client/src/components/api_test.js
+++ client/src/components/api_test.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
-class ApiTest extends React.Component{
-    render(){
-        return(
+class ApiTest extends React.Component {
+
+    getUserData() {
+        axios.get('/api/get-user')
+            .then((res) => console.log('Response from get-user: ', res.data))
+            .catch((err) => console.log('An error has occured: ', err.message));
+    }
+
+    getArticleData(){
+        axios.get('/api/get-article')
+            .then((res)=> console.log('Response from get-article: ', res.data))
+            .catch((err)=> console.log('An error has occured: ', err.message));
+    }
+
+    render() {
+        return (
             <div>
-                <h1>API THUNDER DOME</h1>
-            </div>
+                <h1>WELCOME TO THE MF API THUNDERDOME</h1>
+                <button onClick={this.getUserData}>Get User Info</button>
+                <br/>
+                <button onClick={this.getArticleData}>Get Article Info</button>
+            </div>  
         )
     }
+
 }
 
 export default ApiTest;
